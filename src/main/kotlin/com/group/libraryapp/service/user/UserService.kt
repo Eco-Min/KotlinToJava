f14b64e13a666dc79fd56a936f632ae8019d760c
eco_min <guldor10.b@gmail.com>
2023-02-25 13:44:20 +0900

    service User kotlin 전

---
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -0,0 +1,41 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserResponse
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import javax.persistence.Entity
+
+@Service
+class UserService @Autowired constructor(
+    val userRepository: UserRepository
+) {
+
+    @Transactional
+    fun saveUser(request: UserCreateRequest) {
+        val newUser = User(request.name, request.age)
+        userRepository.save(newUser)
+    }
+
+    @Transactional(readOnly = true)
+    fun getUsers(): List<UserResponse> {
+        return userRepository.findAll().map { UserResponse(it) }.toList()
+    }
+
+    @Transactional
+    fun updateUserName(request: UserUpdateRequest) {
+        val user = userRepository.findById(request.id).orElseThrow(::IllegalArgumentException)
+        user.updateName(request.name)
+    }
+
+    @Transactional
+    fun deleteUser(name: String) {
+        val user = userRepository.findByName(name).orElseThrow(::IllegalArgumentException)
+        userRepository.delete(user)
+    }
+
+}
