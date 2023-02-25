48edd34d01c8aaa15aabf156d36d1c943f566088
eco_min <guldor10.b@gmail.com>
2023-02-25 13:43:43 +0900

    domain user/userLoanHistory kotlin 전

---
+++ src/main/kotlin/com/group/libraryapp/domain/user/User.kt
@@ -0,0 +1,39 @@
+package com.group.libraryapp.domain.user
+
+import com.group.libraryapp.domain.book.Book
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
+import javax.persistence.*
+
+@Entity
+class User(
+    @Column(nullable = false)
+    var name: String,
+
+    val age: Int,
+
+    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val userLoanHistory: MutableList<UserLoanHistory> = mutableListOf(),
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+) {
+    init {
+        if (name.isBlank()) {
+            throw IllegalArgumentException("이름이 비어 있을 수 없습니다.")
+        }
+    }
+
+    fun updateName(name: String) {
+        this.name = name
+    }
+
+    fun loanBook(book: Book) {
+        this.userLoanHistory.add(UserLoanHistory(this, book.name))
+    }
+
+    fun returnBook(bookName: String) {
+        this.userLoanHistory.first { it.bookName == bookName }.doReturn()
+    }
+
+}
